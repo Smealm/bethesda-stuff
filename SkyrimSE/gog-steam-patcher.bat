011cb764abab61de602261d50e859100d9a30491
Smealm <46572305+Smealm@users.noreply.github.com>
2024-11-17 18:31:18 -0500

    Update gog-steam-patcher.bat

--- SkyrimSE/gog-steam-patcher.bat
+++ SkyrimSE/gog-steam-patcher.bat
@@ -3,32 +3,93 @@ echo setting variables
 set arch="x86_64"
 set name="xdelta3"
 set ver="3.1.0"
+:mainmenu
+cls
+
+:: Title
+echo ***************************************
+echo   Skyrim GOG to Skyrim Steam patcher
+echo ***************************************
 echo.
 
-echo downloading patcher
-curl -fSLo xdelta.zip https://github.com/jmacd/xdelta-gpl/releases/download/v%ver%/%name%-%ver%-%arch%.exe.zip
-powershell expand-archive xdelta.zip
-del xdelta.zip
-move xdelta\%name%-%ver%-%arch%.exe
+:: Option Menu
+echo [1] Patch Skyrim
+echo [2] Unpatch Skyrim
+echo [3] Exit
 echo.
 
-echo downloading patch
-curl -fSLo xdelta.zip https://files.catbox.moe/2myxqs.zip
-powershell expand-archive xdelta.zip
-del xdelta.zip
-move xdelta\patch.xdelta
-rmdir xdelta
-echo.
+:: Get User Choice
+choice /c 123 /n /m "Please select an option (1, 2, or 3):"
 
-echo patching skyrim
-ren SkyrimSE.exe SkyrimSE.exe.unpatched
-%name%-%ver%-%arch%.exe -d -s SkyrimSE.exe.unpatched patch.xdelta SkyrimSE.exe
-echo.
+:: Handle User Input
+set choice=%errorlevel%
 
-echo cleaning up leftovers
-del patch.xdelta
-del %name%-%ver%-%arch%.exe
-echo.
+:: Handle Patching Option
+if %choice%==1 (
+    cls
+    echo *********************
+    echo   Patching Skyrim...
+    echo *********************
+    echo.
+    if exist SkyrimSE.exe.unpatched (
+        echo Skyrim is already patched!
+		pause
+        goto mainmenu
+    ) else (
+        echo Skyrim is not patched yet. Proceeding with patching...
+        echo.
+        
+        echo Downloading patcher...
+        curl -fSLo xdelta.zip https://github.com/jmacd/xdelta-gpl/releases/download/v%ver%/%name%-%ver%-%arch%.exe.zip
+        powershell Expand-Archive -Path xdelta.zip -DestinationPath .
+        del xdelta.zip
+        move xdelta\%name%-%ver%-%arch%.exe .
+        echo.
 
-echo Skyrim GOG has been patched!
-pause
+        echo Downloading patch...
+        curl -fSLo xdelta.zip https://files.catbox.moe/nt10nn.zip
+        powershell Expand-Archive -Path xdelta.zip -DestinationPath .
+        del xdelta.zip
+        move xdelta\exepatch.xdelta .
+        move xdelta\dllpatch.xdelta .
+        rmdir /S /Q xdelta
+        echo.
+
+        echo Applying patches...
+        ren SkyrimSE.exe SkyrimSE.exe.unpatched
+        %name%-%ver%-%arch%.exe -d -s SkyrimSE.exe.unpatched exepatch.xdelta SkyrimSE.exe
+        %name%-%ver%-%arch%.exe -d -s Galaxy64.dll dllpatch.xdelta steam_api64.dll
+        echo.
+
+        echo Cleaning up...
+        del exepatch.xdelta
+        del dllpatch.xdelta
+        del %name%-%ver%-%arch%.exe
+        echo.
+
+        echo Skyrim GOG has been patched successfully!
+		pause
+        goto mainmenu
+    )
+)
+
+:: Handle Unpatching Option
+if %choice%==2 (
+    cls
+    echo *********************
+    echo  Unpatching Skyrim...
+    echo *********************
+    echo.
+    if not exist steam_api64.dll (
+        echo Skyrim is already unpatched.
+		pause
+        goto mainmenu
+    ) else (
+        del SkyrimSE.exe
+        ren SkyrimSE.exe.unpatched SkyrimSE.exe
+		del steam_api64.dll
+        echo Skyrim has been unpatched successfully.
+		pause
+        goto mainmenu
+    )
+)
